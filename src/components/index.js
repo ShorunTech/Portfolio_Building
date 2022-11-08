db73c05423290c85d93453b254bee2474678f490
ShorunTech <gshoronke@gmail.com>
2022-11-08 19:43:25 +0100

    index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,9 @@ export { default as Discord } from './Discord';
 export { default as Navbar } from './Navbar';
 export { default as About } from './About';
 export { default as Skills } from './Skills';
+export { default as Portfolio } from './Portfolio';
+
 export { default as Footer } from './Footer';
 export { default as Home } from './Home';
 
-export { default as Portfolio } from './Portfolio';
+
