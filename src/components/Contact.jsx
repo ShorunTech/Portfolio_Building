fefcb436aa0bd1ff7ee64a6971cdafd348c0e578
ShorunTech <gshoronke@gmail.com>
2022-11-08 20:06:36 +0100

    Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,7 +14,9 @@ const Contact = () => {
         
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
+            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
             
+        
         </form>
     </div>
   )
