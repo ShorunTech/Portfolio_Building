968c61684bd979c94b21a89914e86a80ffcd594e
ShorunTech <gshoronke@gmail.com>
2022-11-09 21:53:46 +0100

    portfolio.jsx

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import WorkImg from '../images/workImg.jpeg';
+import realEstate from '../images/realestate.jpg';
+
+const Portfolio = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+            <br/>
+            <br/>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+            Work
+          </p>
+          <p className='py-6'>// Check out some of my recent work</p>
+        </div>
