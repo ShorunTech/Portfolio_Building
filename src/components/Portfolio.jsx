3a093a8e12079dce8b04544f02fcc6f2b3ac66e2
ShorunTech <gshoronke@gmail.com>
2022-11-12 20:50:02 +0100

    portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,13 +11,16 @@ const Portfolio = () => {
             <br/>
             <br/>
 
+            <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+            Work
+          </p>
+          <p className='py-6'>// Check out some of my recent work</p>
+        </div>
+
+
+
           
-
-
-          
-        {/* Container */}
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-
+      
             {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${WorkImg})` }}
