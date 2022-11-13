a519d6f7b44b8c3c7929cc7f811b6840486d6f4b
ShorunTech <gshoronke@gmail.com>
2022-11-13 06:50:46 +0100

    Portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -104,19 +104,7 @@ const Portfolio = () => {
                 React JS Application
               </span>
               <div className='pt-8 text-center'>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
+               
            
             {/* Hover Effects */}
            
