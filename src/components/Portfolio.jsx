0c49b482f4f849bf7de4162accc017ec2d18e473
ShorunTech <gshoronke@gmail.com>
2022-11-14 19:51:21 +0100

    Portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -75,7 +75,6 @@ const Portfolio = () => {
             style={{ backgroundImage: `url(${WorkImg})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
-            {/* Hover Effects */}
           
              
             {/* Hover Effects */}
