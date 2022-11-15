e3f2fe62c9fd6ee1c66316d536015877a110a64b
ShorunTech <gshoronke@gmail.com>
2022-11-15 19:50:06 +0100

    portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,7 +5,7 @@ import realEstate from '../images/realestate.jpg';
 
 const Portfolio = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+    <div name='work' className='w-full gradient-bg-welcome text-gray-300'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
             <br/>
