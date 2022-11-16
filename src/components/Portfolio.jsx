54187755687dbd5d5a8d9e02bc0066185d31091d
ShorunTech <gshoronke@gmail.com>
2022-11-16 16:08:08 +0100

    portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -64,7 +64,11 @@ const Portfolio = () => {
                     Demo
                   </button>
                 </a>
-                
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
                 
               </div>
             </div>
