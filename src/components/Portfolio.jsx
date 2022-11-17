5dde5fa5dab14ef889fca3d550a0d751c460c76f
ShorunTech <gshoronke@gmail.com>
2022-11-17 16:01:04 +0100

    portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -62,13 +62,15 @@ const Portfolio = () => {
               
               <div className='pt-8 text-center'>
                 <a href='/'>
-                  
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
                   
                 </a>
                 <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
+                  
+
+                  
                 </a>
               </div>
             </div>
