0f4afd161bab539c58d2b1ff272025268e083f10
Shoronke Gbenga <gshoronke@gmail.com>
2023-01-13 14:55:35 +0100

    portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -94,9 +94,8 @@ const Portfolio = () => {
                   </button>
                 </a>
                 <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
+                 
+                 
                 </a>
                 
               </div>
