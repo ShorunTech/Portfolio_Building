0ee30719e8cfbc0c42c386bb42a7c519091dbddf
Shoronke Gbenga <gshoronke@gmail.com>
2023-01-18 08:08:34 +0100

    skill.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -52,7 +52,7 @@ const Skills = () => {
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={AWS} alt="HTML icon" />
-                  
+                  <p className='my-4'>AWS</p>
               </div>
           </div>
       </div>
