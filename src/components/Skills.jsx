889fa2f79dad5f0b4a84eda3ffe2dc09f56d4ad1
Shoronke Gbenga <gshoronke@gmail.com>
2023-01-19 22:07:25 +0100

    skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,7 +13,8 @@ import Mongo from '../images/mongo.png';
 
 const Skills = () => {
   return (
-    
+    <div name='skills' className='w-full h-screen  text-gray-300 gradient-bg-about'>
+      {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
               <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
