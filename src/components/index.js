8f8033817394290019b51f6ca552356b86abbe71
Shoronke Gbenga <gshoronke@gmail.com>
2023-01-22 12:43:35 +0100

    index.js

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,6 @@ export { default as Skills } from './Skills';
 export { default as Portfolio } from './Portfolio';
 export { default as Contact } from './Contact';
 export { default as Footer } from './Footer';
-
+export { default as Home } from './Home';
 
 
