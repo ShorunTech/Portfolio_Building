d394aaf1b08b6f1d01c13344ccd2484b6cf94cc3
Shoronke Gbenga <gshoronke@gmail.com>
2023-01-22 12:47:09 +0100

    navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -79,9 +79,7 @@ const Navbar = () => {
         </li>
         <li className='py-6 text-4xl'>
           {' '}
-          <Link onClick={handleClick} to='work' smooth={true} duration={500}>
-            Skills
-          </Link>
+          
         </li>
         <li className='py-6 text-4xl'>
           {' '}
