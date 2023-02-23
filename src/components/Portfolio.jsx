6eca36dc19b8d79f0980ebf9a11f280b2d4a6caf
Shoronke Gbenga <gshoronke@gmail.com>
2023-02-23 14:46:17 +0100

    Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -140,7 +140,9 @@ const Portfolio = () => {
            
           {/* Hover Effects */}
           <div className='opacity-0 group-hover:opacity-100'>
-              
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Application
+              </span>
               <div className='pt-8 text-center'>
                 <a href='/'>
                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
